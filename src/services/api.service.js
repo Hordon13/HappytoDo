acc4c9b36bef498348b9fbeb502c839daca57a40
Horváth Donát <hordon@hordon.local>
2019-10-14 17:05:10 +0200

    outsource the RESTful services into a separate file

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,13 +1,44 @@
-const ApiService = {
-
+export default new class {
   get(url) {
-    let myData = undefined;
-    fetch(url)
+    return fetch(url)
       .then(res => res.json())
-      .then(data => myData = data)
       // eslint-disable-next-line no-console
       .catch(err => console.log(err));
-    return myData;
   }
 
-};
+  post(url, payload) {
+    return fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    })
+      .then(res => res.json())
+      // eslint-disable-next-line no-console
+      .catch(err => console.log(err));
+  }
+
+  put(url, payload) {
+    return fetch(url, {
+      method: "PUT",
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      },
+      body: JSON.stringify(payload)
+    })
+    // eslint-disable-next-line no-console
+      .catch(err => console.log(err));
+  }
+
+  delete(url) {
+    return fetch(url, {
+      method: 'DELETE',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      }
+    })
+    // eslint-disable-next-line no-console
+      .catch(err => console.log(err));
+  }
+}
